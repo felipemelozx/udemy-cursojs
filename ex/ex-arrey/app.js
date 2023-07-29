eaf803fa3bceb658d0d98eabcdac72c6ef2f8675
felipemelozx <127111346+felipemelozx@users.noreply.github.com>
2023-07-29 16:58:07 -0300

    fim do ex de array

--- ex/ex-arrey/app.js
+++ ex/ex-arrey/app.js
@@ -1,8 +1,17 @@
-
-
 function check(){
-    let name = document.querySelector('#iname');
-    let sobrenome = document.querySelector('#iSobreNome')
-    let peso = document.querySelector('#ipeso')
-    let altura = document.querySelector('#ialtura')
+    let name = document.querySelector('#iname').value;
+    let sobrenome = document.querySelector('#iSobreNome').value;
+    let peso = document.querySelector('#ipeso').value;
+    let altura = document.querySelector('#ialtura').value;
+    let resu = document.querySelector('.res')
+
+    let pessoa = {name, sobrenome, peso, altura,}
+    
+    let array = [''];
+    
+    array = (pessoa);
+    
+    console.log(array)
+    resu.innerHTML += `<p>Nome: ${pessoa.name} Sobrenome: ${pessoa.sobrenome}Peso: ${pessoa.peso}Altura: ${pessoa.altura}</p>`
+
 }
