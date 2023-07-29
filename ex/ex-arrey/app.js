0c1534b6d9decf83b2bec7b50db83951239c8923
felipemelozx <127111346+felipemelozx@users.noreply.github.com>
2023-07-29 15:33:47 -0300

    criacao de variaveis

---
+++ ex/ex-arrey/app.js
@@ -0,0 +1,8 @@
+
+
+function check(){
+    let name = document.querySelector('#iname');
+    let sobrenome = document.querySelector('#iSobreNome')
+    let peso = document.querySelector('#ipeso')
+    let altura = document.querySelector('#ialtura')
+}
