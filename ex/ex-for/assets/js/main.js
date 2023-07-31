6d83350ef37b3ff772bc02c3e19aefe43f87009a
felipemelozx <127111346+felipemelozx@users.noreply.github.com>
2023-07-31 12:14:33 -0300

    ex sobre for com array e objeto

---
+++ ex/ex-for/assets/js/main.js
@@ -0,0 +1,16 @@
+const elementos = [
+    { tag: 'p', texto: 'Frase 1' },
+    { tag: 'div', texto: 'Frase 1' },
+    { tag: 'main', texto: 'Frase 1' },
+    { tag: 'section', texto: 'Frase 1' },
+]
+let container = document.querySelector('.container')
+
+
+for (let i = 0; i < elementos.length; i++){
+   let tagName = elementos[i]
+   
+    function criaTag(){ 
+        
+   }
+}
