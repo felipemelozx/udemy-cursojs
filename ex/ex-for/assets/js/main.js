32404b886c1ff334ac01285c9fe9ef738aa18de5
felipemelozx <127111346+felipemelozx@users.noreply.github.com>
2023-07-31 12:22:03 -0300

    ex array fim

--- ex/ex-for/assets/js/main.js
+++ ex/ex-for/assets/js/main.js
@@ -1,16 +1,16 @@
 const elementos = [
     { tag: 'p', texto: 'Frase 1' },
-    { tag: 'div', texto: 'Frase 1' },
-    { tag: 'main', texto: 'Frase 1' },
-    { tag: 'section', texto: 'Frase 1' },
+    { tag: 'div', texto: 'Frase 2' },
+    { tag: 'main', texto: 'Frase 3' },
+    { tag: 'section', texto: 'Frase 4' },
 ]
-let container = document.querySelector('.container')
-
+const container = document.querySelector('.container')
+const div = document.createElement('div')
 
 for (let i = 0; i < elementos.length; i++){
-   let tagName = elementos[i]
-   
-    function criaTag(){ 
-        
-   }
+   const {tag, texto} = elementos[i]
+    let tagCriada = document.createElement('tag')
+    tagCriada.innerHTML = texto
+    div.appendChild(tagCriada)
 }
+container.appendChild(div)
