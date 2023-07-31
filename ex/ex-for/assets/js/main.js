3b898167a4167684679dd1c868f0869b3226625e
felipemelozx <127111346+felipemelozx@users.noreply.github.com>
2023-07-31 12:36:54 -0300

    innertext main js

--- ex/ex-for/assets/js/main.js
+++ ex/ex-for/assets/js/main.js
@@ -10,7 +10,7 @@ const div = document.createElement('div')
 for (let i = 0; i < elementos.length; i++){
    const {tag, texto} = elementos[i]
     let tagCriada = document.createElement('tag')
-    tagCriada.innerHTML = texto
+    tagCriada.innerText = texto
     div.appendChild(tagCriada)
 }
 container.appendChild(div)
